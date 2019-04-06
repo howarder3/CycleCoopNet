c3f5dfe22bd4952319a82b03750326de50634cdc
howarder3 <howarder3@gmail.com>
2019-04-06 20:26:26 +0800

    update 0406

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ python main.py
 import tensorflow as tf
 import os
 
-from model import Cooppix2pix
+from model import Coop_pix2pix
 
 # parameters setting
 tf.app.flags.DEFINE_integer('epoch',200,'how many epochs to train')
 
 # folder position
 tf.app.flags.DEFINE_string('output_dir', './output_dir', 'output directory')
-
+tf.app.flags.DEFINE_string('checkpoint_dir', './checkpoint_dir', 'checkpoint directory')
+tf.app.flags.DEFINE_string('log_dir', './log_dir', 'log directory')
 
 
 
@@ -26,23 +27,40 @@ FLAGS = tf.app.flags.FLAGS
 
 def main(_):
 	output_dir = FLAGS.output_dir
+	checkpoint_dir = FLAGS.checkpoint_dir
+	log_dir = FLAGS.log_dir
 
 	with tf.Session() as sess:
 		if tf.gfile.Exists(output_dir):
-			user_input = input('Warning! Output folder exists! Enter \'y\' to delete folder! ')
+			user_input = input('Warning! Output directory exists! Enter \'y\' to delete folder! ')
 			if user_input == 'y':
 				tf.gfile.DeleteRecursively(output_dir)
 				tf.gfile.MakeDirs(output_dir)
 		else:
 			tf.gfile.MakeDirs(output_dir)
 
-	# if not os.path.exists(args.checkpoint_dir):
-	# 	os.makedirs(args.checkpoint_dir)
-	# 	if not os.path.exists(args.sample_dir):
-	# 		os.makedirs(args.sample_dir)
-	# 		if not os.path.exists(args.test_dir):
-	# 			os.makedirs(args.test_dir)
-	# 			print("test")
+		if tf.gfile.Exists(checkpoint_dir):
+			user_input = input('Warning! Checkpoint directory exists! Enter \'y\' to delete folder! ')
+			if user_input == 'y':
+				tf.gfile.DeleteRecursively(checkpoint_dir)
+				tf.gfile.MakeDirs(checkpoint_dir)
+		else:
+			tf.gfile.MakeDirs(checkpoint_dir)
+
+		if tf.gfile.Exists(log_dir):
+			user_input = input('Warning! Log directory exists! Enter \'y\' to delete folder! ')
+			if user_input == 'y':
+				tf.gfile.DeleteRecursively(log_dir)
+				tf.gfile.MakeDirs(log_dir)
+		else:
+			tf.gfile.MakeDirs(log_dir)
+
+
+		model = Coop_pix2pix(sess, epoch=200, output_dir=FLAGS.output_dir, checkpoint_dir=FLAGS.checkpoint_dir, log_dir=FLAGS.log_dir)
+
+
+		model.train(sess)
+
 
 
 # program start here
--- model.py
+++ model.py
@@ -2,19 +2,23 @@ import tensorflow as tf
 import numpy as np
 
 
-class Cooppix2pix(object):
-	def __init__(self, sess, epoch=200, checkpoint_dir="./checkpoint_dir", sample_dir="./sample_dir",test_dir="./test_dir"):
+class Coop_pix2pix(object):
+	def __init__(self, sess, epoch=200, output_dir="./output_dir", checkpoint_dir="./checkpoint_dir", log_dir="./log_dir"):
 		"""
 		args:
 			sess: tensorflow session
 
 		"""
 
-		
+
 		self.sess = sess
 		self.epoch = epoch
 
-
+		self.output_dir = output_dir
 		self.checkpoint_dir = checkpoint_dir
-		self.sample_dir = sample_dir
-		self.test_dir = test_dir
+		self.log_dir = log_dir
+
+	def train(self,sess):
+		print("Now training model......")
+
+
