eab0bf2d84d01edcb7b789d6b4bdda13a786e558
howarder3 <howarder3@gmail.com>
2019-04-07 05:31:58 +0800

    update 0407

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ from model import Coop_pix2pix
 # parameters setting
 tf.app.flags.DEFINE_integer('epoch',200,'how many epochs to train')
 
+
+# dataset_name
+tf.app.flags.DEFINE_string('dataset_name', 'facades', 'dataset name')
 # folder position
+tf.app.flags.DEFINE_string('dataset_dir', './dataset_dir', 'dataset directory')
 tf.app.flags.DEFINE_string('output_dir', './output_dir', 'output directory')
 tf.app.flags.DEFINE_string('checkpoint_dir', './checkpoint_dir', 'checkpoint directory')
 tf.app.flags.DEFINE_string('log_dir', './log_dir', 'log directory')
@@ -26,37 +30,43 @@ FLAGS = tf.app.flags.FLAGS
 
 
 def main(_):
+	dataset_dir = FLAGS.dataset_dir+"/"+FLAGS.dataset_name
 	output_dir = FLAGS.output_dir
 	checkpoint_dir = FLAGS.checkpoint_dir
 	log_dir = FLAGS.log_dir
 
 	with tf.Session() as sess:
+		if tf.gfile.Exists(dataset_dir):
+			print("\nError! Dataset not found!\n")
+			return
+			
 		if tf.gfile.Exists(output_dir):
-			user_input = input('Warning! Output directory exists! Enter \'y\' to delete folder! ')
-			if user_input == 'y':
-				tf.gfile.DeleteRecursively(output_dir)
-				tf.gfile.MakeDirs(output_dir)
+			# user_input = input('\nWarning! Output directory exists! Enter \'y\' to delete folder!\n')
+			# if user_input == 'y':
+			tf.gfile.DeleteRecursively(output_dir)
+			tf.gfile.MakeDirs(output_dir)
 		else:
 			tf.gfile.MakeDirs(output_dir)
 
 		if tf.gfile.Exists(checkpoint_dir):
-			user_input = input('Warning! Checkpoint directory exists! Enter \'y\' to delete folder! ')
-			if user_input == 'y':
-				tf.gfile.DeleteRecursively(checkpoint_dir)
-				tf.gfile.MakeDirs(checkpoint_dir)
+			# user_input = input('\nWarning! Checkpoint directory exists! Enter \'y\' to delete folder!\n')
+			# if user_input == 'y':
+			tf.gfile.DeleteRecursively(checkpoint_dir)
+			tf.gfile.MakeDirs(checkpoint_dir)
 		else:
 			tf.gfile.MakeDirs(checkpoint_dir)
 
 		if tf.gfile.Exists(log_dir):
-			user_input = input('Warning! Log directory exists! Enter \'y\' to delete folder! ')
-			if user_input == 'y':
-				tf.gfile.DeleteRecursively(log_dir)
-				tf.gfile.MakeDirs(log_dir)
+			# user_input = input('\nWarning! Log directory exists! Enter \'y\' to delete folder!\n')
+			# if user_input == 'y':
+			tf.gfile.DeleteRecursively(log_dir)
+			tf.gfile.MakeDirs(log_dir)
 		else:
 			tf.gfile.MakeDirs(log_dir)
 
 
-		model = Coop_pix2pix(sess, epoch=200, output_dir=FLAGS.output_dir, checkpoint_dir=FLAGS.checkpoint_dir, log_dir=FLAGS.log_dir)
+		model = Coop_pix2pix(sess, epoch=200, dataset_name=FLAGS.dataset_name, 
+			output_dir=FLAGS.output_dir, checkpoint_dir=FLAGS.checkpoint_dir, log_dir=FLAGS.log_dir)
 
 
 		model.train(sess)
--- model.py
+++ model.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import numpy as np
+import time
 
+from glob import glob
 
 class Coop_pix2pix(object):
-	def __init__(self, sess, epoch=200, output_dir="./output_dir", checkpoint_dir="./checkpoint_dir", log_dir="./log_dir"):
+	def __init__(self, sess, epoch = 200, dataset_name = 'facades',
+				output_dir='./output_dir', checkpoint_dir='./checkpoint_dir', log_dir='./log_dir'):
 		"""
 		args:
 			sess: tensorflow session
@@ -14,11 +17,29 @@ class Coop_pix2pix(object):
 		self.sess = sess
 		self.epoch = epoch
 
+
+		self.dataset_name = dataset_name
 		self.output_dir = output_dir
 		self.checkpoint_dir = checkpoint_dir
 		self.log_dir = log_dir
 
+	def build_model(self):
+		pass
+
+
+
 	def train(self,sess):
-		print("Now training model......")
+		self.build_model()
+		start_time = time.time()	
+		print("time: {:.6f} , Start training model......".format(0))
+			
+
+		
+
+		# prepare training data
+		training_data = glob('./datasets/{}/train/*.jpg'.format(self.dataset_name))
+		print("time: {:.6f} , Loading data finished! ".format(time.time() - start_time))
+
+
 
 
