a4532e82ff0a000b1d05f87146df92378d5885f8
howard <howarder3@gmail.com>
2019-04-11 05:01:41 +0800

    linearization

--- model.py
+++ model.py
@@ -389,62 +389,63 @@ class Coop_pix2pix(object):
 
 			return generator_output
 
-	def descriptor(self, input_image, reuse=False):
-		with tf.variable_scope('des', reuse=reuse):
-
-			# print("\n------  descriptor layers  ------\n")
-
-			conv1 = conv2d(input_image, 64, kernal=(5, 5), strides=(2, 2), padding="SAME", activate_fn=leaky_relu,
-				name="conv1")
-
-			conv2 = conv2d(conv1, 128, kernal=(3, 3), strides=(2, 2), padding="SAME", activate_fn=leaky_relu,
-				name="conv2")
-
-			conv3 = conv2d(conv2, 256, kernal=(3, 3), strides=(1, 1), padding="SAME", activate_fn=leaky_relu,
-				name="conv3")
-
-			# conv3_reshape = tf.reshape(conv3, [self.batch_size, 1, 1, -1])
-			# print(conv3_reshape.shape)
-			# conv3_reshape.get_shape()[-1] = (1, 1, 1, 1048576)
-
-			fc = fully_connected(conv3, 100, name="fc")
-
-			return fc
-
-		# def descriptor(self, input_image, reuse=False):
+	# def descriptor(self, input_image, reuse=False):
 	# 	with tf.variable_scope('des', reuse=reuse):
 
-	# 		num_filter = 64
+	# 		# print("\n------  descriptor layers  ------\n")
 
-	# 		# ---------- descriptor part ----------
-	# 		# descriptor_conv2d(input_image, output_dimension (by how many filters), scope_name)
-	# 		# input image = [batch_size, 256, 256, input_pic_dim]
+	# 		conv1 = conv2d(input_image, 64, kernal=(5, 5), strides=(2, 2), padding="SAME", activate_fn=leaky_relu,
+	# 			name="conv1")
 
-	# 		# des_layer_0_conv = (batch_size, 128, 128, num_filter)
-	# 		des_layer_0_conv = descriptor_conv2d(input_image, num_filter, name='des_layer_0_conv')
+	# 		conv2 = conv2d(conv1, 128, kernal=(3, 3), strides=(2, 2), padding="SAME", activate_fn=leaky_relu,
+	# 			name="conv2")
 
-	# 		# des_layer_1_conv = (batch_size, 64, 64, num_filter*2)
-	# 		des_layer_1_conv = descriptor_conv2d(leaky_relu(des_layer_0_conv), num_filter*2, name='des_layer_1_conv')
-	# 		des_layer_1_batchnorm = self.descriptor_batchnorm_layer1(des_layer_1_conv)
+	# 		conv3 = conv2d(conv2, 256, kernal=(3, 3), strides=(1, 1), padding="SAME", activate_fn=leaky_relu,
+	# 			name="conv3")
 
-	# 		# des_layer_2_conv = (batch_size, 32, 32, num_filter*4)
-	# 		des_layer_2_conv = descriptor_conv2d(leaky_relu(des_layer_1_batchnorm), num_filter*4, name='des_layer_2_conv')
-	# 		des_layer_2_batchnorm = self.descriptor_batchnorm_layer2(des_layer_2_conv)
-			
-	# 		# des_layer_3_conv = (batch_size, 16, 16, num_filter*8)
-	# 		des_layer_3_conv = descriptor_conv2d(leaky_relu(des_layer_2_batchnorm), num_filter*8, name='des_layer_3_conv')
-	# 		des_layer_3_batchnorm = self.descriptor_batchnorm_layer3(des_layer_3_conv)
+	# 		# conv3_reshape = tf.reshape(conv3, [self.batch_size, 1, 1, -1])
+	# 		# print(conv3_reshape.shape)
+	# 		# conv3_reshape.get_shape()[-1] = (1, 1, 1, 1048576)
 
-	# 		# linearization the descriptor result
-	# 		des_layer_3_reshape = tf.reshape(leaky_relu(des_layer_3_batchnorm), [self.batch_size, -1])
-	# 		# des_layer_3_linearization = linearization(des_layer_3_reshape, 1, 'des_layer_3_linearization')
-	# 		# print(des_layer_3_batchnorm.shape) # (1, 16, 16, 512)
-	# 		# print(des_layer_3_reshape.shape) # (1, 131072)
+	# 		fc = fully_connected(conv3, 100, name="fc")
+
+	# 		return fc
+
+	def descriptor(self, input_image, reuse=False):
+	with tf.variable_scope('des', reuse=reuse):
+
+		num_filter = 64
+
+		# ---------- descriptor part ----------
+		# descriptor_conv2d(input_image, output_dimension (by how many filters), scope_name)
+		# input image = [batch_size, 256, 256, input_pic_dim]
+
+		# des_layer_0_conv = (batch_size, 128, 128, num_filter)
+		des_layer_0_conv = descriptor_conv2d(input_image, num_filter, name='des_layer_0_conv')
+
+		# des_layer_1_conv = (batch_size, 64, 64, num_filter*2)
+		des_layer_1_conv = descriptor_conv2d(leaky_relu(des_layer_0_conv), num_filter*2, name='des_layer_1_conv')
+		des_layer_1_batchnorm = self.descriptor_batchnorm_layer1(des_layer_1_conv)
+
+		# des_layer_2_conv = (batch_size, 32, 32, num_filter*4)
+		des_layer_2_conv = descriptor_conv2d(leaky_relu(des_layer_1_batchnorm), num_filter*4, name='des_layer_2_conv')
+		des_layer_2_batchnorm = self.descriptor_batchnorm_layer2(des_layer_2_conv)
+		
+		# des_layer_3_conv = (batch_size, 16, 16, num_filter*8)
+		des_layer_3_conv = descriptor_conv2d(leaky_relu(des_layer_2_batchnorm), num_filter*8, name='des_layer_3_conv')
+		des_layer_3_batchnorm = self.descriptor_batchnorm_layer3(des_layer_3_conv)
+
+		# linearization the descriptor result
+		des_layer_3_reshape = tf.reshape(leaky_relu(des_layer_3_batchnorm), [self.batch_size, -1])
+		des_layer_3_linearization = linearization(des_layer_3_reshape, 1, 'des_layer_3_linearization')
+		# print(des_layer_3_batchnorm.shape) # (1, 16, 16, 512)
+		# print(des_layer_3_reshape.shape) # (1, 131072)
+		print("des_layer_3_linearization: ",des_layer_3_linearization.shape)
 
 
-	# 		# input image = [batch_size, 256, 256, input_pic_dim]
+		# input image = [batch_size, 256, 256, input_pic_dim]
 
-	# 		return des_layer_3_reshape
+		return des_layer_3_linearization
 
 
 	def langevin_dynamics_descriptor(self, input_image_arg):
